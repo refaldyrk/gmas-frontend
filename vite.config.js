54bb39a9e3e0c89dcbd803631d93c77ddffcb402
Refaldy <test@tests-MacBook-Pro.local>
2024-12-18 16:26:44 +0700

    ref: port

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		host: 'gmas.efal.lol',
+		port: 2245, // Your server port
+		hmr: {
+			host: 'gmas.efal.lol',
+			port: 2245 // HMR port
+		},
+		cors: true
+	},
 	plugins: [sveltekit()]
 });
